594419a5dfe6e1eebb4a1da2fd7da446e7cab656
bakasifu <156412177+bakanura@users.noreply.github.com>
2026-08-25 18:05:11 +0200

    Fix error message for unsupported distributions

--- auto-install.sh
+++ auto-install.sh
@@ -45,6 +45,7 @@ if [ -n "$(grep -i nixos </etc/os-release)" ]; then
 else
     echo "$ERROR This is not NixOS or the distribution information is not available."
     exit 1
+fi
 
 nhl_ensure_required_packages
 
